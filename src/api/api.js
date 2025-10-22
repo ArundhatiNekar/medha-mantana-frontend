923b4a86dd8cfe54524ca9f22dab355f436ff55b
nm <nm@gamil.com>
2025-10-22 18:09:16 +0530

    Updated API base URL and Google Client ID

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Create axios instance
-const API = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL, // backend base URL
 });
 
@@ -25,4 +25,4 @@ export const updateProfile = async (profileData) => {
   }
 };
 
-export default API;
+export default api;
